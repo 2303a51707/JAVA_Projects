b4775283287e601c23093e70d3959bbed0ef201a
2303a51707 <2303a51707@sru.edu.in>
2026-06-21 13:10:41 +0530

    Add files via upload

---
+++ Online_Banking_System/src/main/java/com/nexbank/nexonlinebanking/MySQLConnector.java
@@ -0,0 +1,16 @@
+package com.nexbank.nexonlinebanking;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class MySQLConnector {
+
+    public static Connection connectDB() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            return DriverManager.getConnection("jdbc:mysql://sql11.freesqldatabase.com:3306/sql11589964", "sql11589964", "UnAPsV6nsM");
+        } catch (Exception e) {
+            return null;
+        }
+    }
+}
